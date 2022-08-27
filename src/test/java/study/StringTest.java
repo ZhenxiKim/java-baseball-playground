0b672db3a92c156205671d6c74abb6e8b40b5844
zhenxiKim <8055kjh@gmail.com>
2022-08-28 00:22:48 +0900

    String Test 요구사항1 - charAt 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,7 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIOException;
 
 public class StringTest {
     @Test
@@ -22,6 +25,26 @@ public class StringTest {
         String str = "(1,2)";
         String result = str.substring(1, str.length()-1);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    /*    @Test
+    @DisplayName("특정문자 위치 확인 테스트")
+    void requirement3() {
+        int index = 1;
+        String str = "abc";
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> str.charAt(index));
+                //.withMessageContaining("%d", index);
+    }*/
+
+
+    @Test
+    @DisplayName("Exception 테스트")
+    public void testException() {
+        assertThatIOException().isThrownBy(() -> {throw new IOException("boom!"); })
+                .withMessage("%s!", "boom")
+                .withMessageContaining("boom")
+                .withNoCause();
     }
 
 }
