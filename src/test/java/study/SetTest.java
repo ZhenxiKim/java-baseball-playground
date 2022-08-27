5dd0f0131c6a00c300d798ea3e3c4587418c389d
zhenxiKim <8055kjh@gmail.com>
2022-08-28 00:27:50 +0900

    Set Collection Test 요구사항2 - contains() 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,7 +1,11 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.util.HashSet;
 import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,8 +27,17 @@ public class SetTest {
     }
 
     @Test
+    @DisplayName("set size 확인")
     void requirement1() {
         int size = numbers.size();
         assertThat(size).isEqualTo(4);
+    }
+
+    @DisplayName("set 값 존재 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void requirement2(int input) {
+        //중복되는 테스트 코드 ParameterizedTest
+        assertThat(numbers.contains(input));
     }
 }
